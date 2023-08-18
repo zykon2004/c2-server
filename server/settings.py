39c4f710d9514692fc540493237a3f7458cb154f
Robert <zykon@pm.me>
2023-08-18 05:00:30 +0300

    cleaing up

--- server/settings.py
+++ server/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiL
 
 SERVER_PORT = 8080
 REQUEST_TIMEOUT = 5
-CLIENT_LIVELINESS_THRESHOLD_MINUTES = 600
+CLIENT_LIVELINESS_THRESHOLD_MINUTES = 1
 DB_LOCATION = Path(__file__).resolve().parents[1] / "resources" / "db.sqlite3"
 DB_ENGINE = f"sqlite:///{DB_LOCATION}"
 PROTOCOL: Literal["http", "https"] = "http"
